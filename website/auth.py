7ca6a8f82634de02c1edf08b971465d703fb8176
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-18 08:35:37 +0100

    add message flashing

--- website/auth.py
+++ website/auth.py
@@ -1,39 +1,44 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login', method=['POST', 'GET'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
 
-        return redirect(url_for('home'))
+        return redirect(url_for('views.home'))
 
     return render_template('login.html')
 
 
-@auth.route('/logout', method=['POST', 'GET'])
+@auth.route('/logout')
 def logout():
     return '<h1>Logout</h1>'
 
 
-@auth.route('/sign-up', method=['POST', 'GET'])
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         email = request.form.get('email')
         name = request.form.get('name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        if len(name) < 2:
-            pass
+        if len(email) < 5:
+            flash('Email is too short! Must be greater than 4 characters.',
+                  category='error')
+        elif len(name) < 2:
+            flash('Name is too short! Must be greater than 1 character.',
+                  category='error')
         elif password1 != password2:
-            pass
-        elif password1 < 8:
-            pass
-        elif len(email) < 5:
-            pass
+            flash('Passwords are not the same!', category='error')
+        elif len(password1) < 8:
+            flash('Password is too short! Must be greater than 7 '
+                  'characters', category='error')
         else:
-            return redirect(url_for('home'))
+            flash('User created successfully!', category='success')
+            return redirect(url_for('views.home'))
     return render_template('sign_up.html')
