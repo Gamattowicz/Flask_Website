2fd3408127c213d8052fd643e92c6f22bc49275a
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-18 13:45:56 +0100

    add flask_login module

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 
 auth = Blueprint('auth', __name__)
@@ -17,6 +18,7 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password!', category='error')
@@ -27,8 +29,10 @@ def login():
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return '<h1>Logout</h1>'
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -59,6 +63,7 @@ def signup():
                             password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('User created successfully!', category='success')
             return redirect(url_for('views.home'))
     return render_template('sign_up.html')
