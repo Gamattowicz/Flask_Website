4820607d1bb8b5f7a6e969358b8a147c98e86c8f
Gamattowicz <p.romanczuk31@gmail.com>
2021-03-18 13:08:15 +0100

    add creating new user accounts

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 
 auth = Blueprint('auth', __name__)
@@ -39,6 +42,11 @@ def signup():
             flash('Password is too short! Must be greater than 7 '
                   'characters', category='error')
         else:
+            new_user = User(email=email, name=name,
+                            password=generate_password_hash(password1,
+                            method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('User created successfully!', category='success')
             return redirect(url_for('views.home'))
     return render_template('sign_up.html')
